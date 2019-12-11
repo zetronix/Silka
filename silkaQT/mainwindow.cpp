196613d9502436d88c554b754b8c7b10c1ce666f
zetronix <47883307+zetronix@users.noreply.github.com>
2019-12-11 14:20:19 +0100

    update mainwindow.cpp
    add debug

--- silkaQT/mainwindow.cpp
+++ silkaQT/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     calendar = new MyCalendar(this);
     calendar->setGeometry(0,0,500,500);
+    qDebug()<<"Insert calendar";
 }
 
 MainWindow::~MainWindow()
