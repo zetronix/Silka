628e8718e0b15d97e2d5b1a7fa3b2889e9015f1e
Zetron <zetron93@gmail.com>
2019-12-17 18:51:34 +0100

    update ubuntu - add workout class

--- build-silka-Desktop-Debug/moc_mycalendar.cpp
+++ build-silka-Desktop-Debug/moc_mycalendar.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MyCalendar_t {
-    QByteArrayData data[3];
-    char stringdata0[24];
+    QByteArrayData data[8];
+    char stringdata0[61];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,11 +33,17 @@ struct qt_meta_stringdata_MyCalendar_t {
 static const qt_meta_stringdata_MyCalendar_t qt_meta_stringdata_MyCalendar = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MyCalendar"
-QT_MOC_LITERAL(1, 11, 11), // "updateLabel"
-QT_MOC_LITERAL(2, 23, 0) // ""
+QT_MOC_LITERAL(1, 11, 10), // "updateList"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 9), // "paintCell"
+QT_MOC_LITERAL(4, 33, 9), // "QPainter*"
+QT_MOC_LITERAL(5, 43, 7), // "painter"
+QT_MOC_LITERAL(6, 51, 4), // "rect"
+QT_MOC_LITERAL(7, 56, 4) // "date"
 
     },
-    "MyCalendar\0updateLabel\0"
+    "MyCalendar\0updateList\0\0paintCell\0"
+    "QPainter*\0painter\0rect\0date"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +53,7 @@ static const uint qt_meta_data_MyCalendar[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,10 +61,12 @@ static const uint qt_meta_data_MyCalendar[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   19,    2, 0x08 /* Private */,
+       1,    1,   24,    2, 0x08 /* Private */,
+       3,    3,   27,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QDate,    2,
+    QMetaType::Void, 0x80000000 | 4, QMetaType::QRect, QMetaType::QDate,    5,    6,    7,
 
        0        // eod
 };
@@ -69,14 +77,15 @@ void MyCalendar::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MyCalendar *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->updateLabel((*reinterpret_cast< const QDate(*)>(_a[1]))); break;
+        case 0: _t->updateList((*reinterpret_cast< const QDate(*)>(_a[1]))); break;
+        case 1: _t->paintCell((*reinterpret_cast< QPainter*(*)>(_a[1])),(*reinterpret_cast< const QRect(*)>(_a[2])),(*reinterpret_cast< const QDate(*)>(_a[3]))); break;
         default: ;
         }
     }
 }
 
 QT_INIT_METAOBJECT const QMetaObject MyCalendar::staticMetaObject = { {
-    &QWidget::staticMetaObject,
+    &QCalendarWidget::staticMetaObject,
     qt_meta_stringdata_MyCalendar.data,
     qt_meta_data_MyCalendar,
     qt_static_metacall,
@@ -95,22 +104,22 @@ void *MyCalendar::qt_metacast(const char *_clname)
     if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_MyCalendar.stringdata0))
         return static_cast<void*>(this);
-    return QWidget::qt_metacast(_clname);
+    return QCalendarWidget::qt_metacast(_clname);
 }
 
 int MyCalendar::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = QWidget::qt_metacall(_c, _id, _a);
+    _id = QCalendarWidget::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
--- build-silka-Desktop-Debug/moc_myhumanoid.cpp
+++ build-silka-Desktop-Debug/moc_myhumanoid.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MyHumanoid_t {
-    QByteArrayData data[1];
-    char stringdata0[11];
+    QByteArrayData data[6];
+    char stringdata0[56];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,16 @@ struct qt_meta_stringdata_MyHumanoid_t {
     )
 static const qt_meta_stringdata_MyHumanoid_t qt_meta_stringdata_MyHumanoid = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "MyHumanoid"
+QT_MOC_LITERAL(0, 0, 10), // "MyHumanoid"
+QT_MOC_LITERAL(1, 11, 14), // "humanoidPushed"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 11), // "setBodyPart"
+QT_MOC_LITERAL(4, 39, 7), // "QLabel*"
+QT_MOC_LITERAL(5, 47, 8) // "bodyPart"
 
     },
-    "MyHumanoid"
+    "MyHumanoid\0humanoidPushed\0\0setBodyPart\0"
+    "QLabel*\0bodyPart"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,22 +51,46 @@ static const uint qt_meta_data_MyHumanoid[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    1,   25,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+
        0        // eod
 };
 
 void MyHumanoid::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MyHumanoid *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->humanoidPushed(); break;
+        case 1: _t->setBodyPart((*reinterpret_cast< QLabel*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLabel* >(); break;
+            }
+            break;
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject MyHumanoid::staticMetaObject = { {
@@ -89,6 +119,17 @@ void *MyHumanoid::qt_metacast(const char *_clname)
 int MyHumanoid::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
